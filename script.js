70bd552207b05bbe7cd196898114d14aa43c9d5e
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-01-18 23:33:09 -0500

    js code input

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Assignment Code, define variables used
+let lowerCase = ('');
+let upperCase = ('');
+let specChar = ('');
+let numChar =  ('');
+let charSet = ('');
+const lowerCaseVal = ['abcdefghijklmnopqrstuvwxyz'];
+const upperCaseVal = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+const specCharVal = ['!@#$%^&*()-_=+{}<>?/']; 
+const numCharVal = ['0123456789'];
+let generateBtn = document.querySelector("#generate");
 
+//create random password
+function generatePassword() {
+  let passVal = prompt('Please enter how long your password will be: min-8 max 120');
+  console.log(passVal);
+  //check what the user wants to use
+  let lowerCase = confirm('Would you like to incorporate lower case letters?');
+    console.log(lowerCase);
+  let upperCase = confirm('Would you like to incorporate upper case letters?');
+    console.log(upperCase);
+  let specChar = confirm('Would you like to use special characters?');
+    console.log(specChar);
+  let numChar = confirm('Would you like to use numbers?');
+    console.log(numChar);
+  
+    //create a variable based on results for charmap
+    //splice values from boolean
+  let charSet(lowerCase && upperCase && numChar && specChar)
+    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_[]{};:<>/?')
+  }charSet.every(lowerCase && upperCase && numChar) 
+    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+   charSet.every(lowerCase && upperCase) 
+    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+   charSet.every(lowerCase) 
+    charSet = ('abcdefghijklmnopqrstuvwxyz')
+   charSet.every(lowerCase && upperCase && specChar) 
+    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_[]{};:<>/?')
+   charSet.every(lowerCase && numChar && specChar) 
+    charSet = ('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_[]{};:<>/?')
+   charSet.every(lowerCase && numChar) 
+    charSet = ('abcdefghijklmnopqrstuvwxyz0123456789')
+   charSet.every(lowerCase && specChar) 
+    charSet = ('abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_[]{};:<>/?');
+  
+
+  console.log(charSet);
+
+  charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+  retVal = "";
+  for (var i = 0, n = charset.length; i < length; i++) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
