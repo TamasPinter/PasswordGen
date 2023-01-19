096b04a8bd01c361202689589e3b257c37b063af
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-01-19 09:46:51 -0500

    finished core script

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ let upperCase = ('');
 let specChar = ('');
 let numChar =  ('');
 let charSet = ('');
-const lowerCaseVal = ['abcdefghijklmnopqrstuvwxyz'];
-const upperCaseVal = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-const specCharVal = ['!@#$%^&*()-_=+{}<>?/']; 
-const numCharVal = ['0123456789'];
+const lowerCaseVal = ('abcdefghijklmnopqrstuvwxyz');
+const upperCaseVal = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+const specCharVal = ('!@#$%^&*()-_=+{}<>?/'); 
+const numCharVal = ('0123456789');
 let generateBtn = document.querySelector("#generate");
 
 //create random password
 function generatePassword() {
+  charSet = ('');
   let passVal = prompt('Please enter how long your password will be: min-8 max 120');
   console.log(passVal);
+  if (isNaN(passVal)) {
+    alert("You haven't entered a number!");
+    return generatePassword();
+  };
+    if (passVal < 8 || passVal > 120) {
+      alert('Please choose between 8 and 120');
+      return generatePassword();
+    };
+  
+
+  
   //check what the user wants to use
   let lowerCase = confirm('Would you like to incorporate lower case letters?');
     console.log(lowerCase);
@@ -25,31 +37,54 @@ function generatePassword() {
     console.log(numChar);
   
     //create a variable based on results for charmap
-    //splice values from boolean
-  let charSet(lowerCase && upperCase && numChar && specChar)
-    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_[]{};:<>/?')
-  }charSet.every(lowerCase && upperCase && numChar) 
-    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-   charSet.every(lowerCase && upperCase) 
-    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-   charSet.every(lowerCase) 
-    charSet = ('abcdefghijklmnopqrstuvwxyz')
-   charSet.every(lowerCase && upperCase && specChar) 
-    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_[]{};:<>/?')
-   charSet.every(lowerCase && numChar && specChar) 
-    charSet = ('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_[]{};:<>/?')
-   charSet.every(lowerCase && numChar) 
-    charSet = ('abcdefghijklmnopqrstuvwxyz0123456789')
-   charSet.every(lowerCase && specChar) 
-    charSet = ('abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_[]{};:<>/?');
+    
+  if(lowerCase === true) {
+    charSet += lowerCaseVal;
+    console.log(charSet)
+  }
+  if(upperCase === true) {
+    charSet += upperCaseVal;
+      console.log(charSet);
+  }
+  if(specChar === true) {
+      charSet += specCharVal;
+      console.log(charSet);
+  }
+  if(numChar === true) {
+      charSet += numCharVal;
+      console.log(charSet);
+      
+    
+   
   
+  /*if(charSet = lowerCase && upperCase && numChar && specChar) {
+    charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_[]{};:<>/?')
+      if(charSet = lowerCase && upperCase && numChar) {
+        charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+        if(charSet = lowerCase && upperCase){
+          charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+          if(charSet = lowerCase){
+            charSet = ('abcdefghijklmnopqrstuvwxyz')
+          }
+        }
+      }else if(charSet = lowerCase && upperCase && specChar){
+        charSet = ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_[]{};:<>/?')
+      }else if(charSet = lowerCase && numChar && specChar) {
+        charSet = ('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_[]{};:<>/?')
+      }else if(charSet = lowerCase && numChar){
+        charSet = ('abcdefghijklmnopqrstuvwxyz0123456789')
+      }else if(charSet = lowerCase && specChar){
+        charSet = ('abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_[]{};:<>/?')
+      }
+  }*/
 
+  }
   console.log(charSet);
 
-  charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+  //charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
   retVal = "";
-  for (var i = 0, n = charset.length; i < length; i++) {
-    retVal += charset.charAt(Math.floor(Math.random() * n));
+  for (var i = 0, n = charSet.length; i < passVal; i++) {
+    retVal += charSet.charAt(Math.floor(Math.random() * n));
   }
   return retVal;
 }
